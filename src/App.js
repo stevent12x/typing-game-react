28d1f1bd3aeecbdac9b33fb6d88d78d6e9ceba34
Steven Turner <steven.turner12x@gmail.com>
2019-12-09 17:17:34 -0600

    core game functions working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { generate } from "./utils/words";
@@ -7,16 +7,49 @@ import useKeyPress from "./hooks/useKeyPress";
 const initialWords = generate();
 
 function App() {
+  const [leftPadding, setLeftPadding] = useState(
+      new Array(20).fill(' ').join(''),
+  );
 
   useKeyPress(key => {
+    let updatedOutgoingChars = outgoingChars;
+    let updatedIncomingChars = incomingChars;
+
+    if (key === currentChar) {
+      if (leftPadding.length > 0) {
+        setLeftPadding(leftPadding.substr(1))
+      }
+      updatedOutgoingChars += currentChar;
+      setOutgoingChars(updatedOutgoingChars);
+
+      setCurrentChar(incomingChars.charAt(0));
+
+      updatedIncomingChars = incomingChars.substr(1);
+      if (updatedIncomingChars.split('').length < 10) {
+        updatedIncomingChars += '' + generate()
+      }
+      setIncomingChars(updatedIncomingChars);
+    }
   });
+
+  const [outgoingChars, setOutgoingChars] = useState('');
+  const [currentChar, setCurrentChar] = useState(initialWords.charAt(0));
+  const [incomingChars, setIncomingChars] = useState(initialWords.substr(1));
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="Character">
+          <span className="Character-out">
+            {(leftPadding + outgoingChars).slice(-20)}
+          </span>
+          <span className="Character-current">
+            {currentChar}
+          </span>
+          <span>
+            {incomingChars.substr(0, 20)}
+          </span>
         </p>
         <a
           className="App-link"
